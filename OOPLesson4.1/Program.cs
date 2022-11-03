a23a91f1fd080e21bc99e78e1c9849e1e99b9ca8
spaceshipwalls@gmail.com <spaceshipwalls@gmail.com>
2022-11-03 18:33:14 +0200

    Add project files.

---
+++ OOPLesson4.1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //אבא  base 
+            Person person = new Person()
+            {
+                FirstName = "sdf",
+                LastName = "sdf"
+            };
+
+
+            //ילד  Derived
+            Students st = new Students
+            {
+                FirstName = "dd",
+                Grade = 13,
+                LastName = "sdf"
+            };
+
+            //Dervied נכד
+            SuperStudents super = new SuperStudents
+            {
+                Age = 30,
+                FirstName = "sdf",
+                Grade = 30,
+                LastName = "sdfsd"
+            };
+            Console.WriteLine($"{st.LastName},{st.FirstName},{st.Grade}");
+
+
+
+
+        }
+    }
+
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Students : Person
+    {
+        public int Grade { get; set; }
+        //public Students(int grade)
+        //{
+        //    Grade = grade;
+        //}
+    }
+
+    class SuperStudents : Students
+    {
+        public int Age { get; set; }
+
+    }
+}
